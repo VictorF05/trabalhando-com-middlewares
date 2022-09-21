bcb92a17766b9686766f4e4b9b25d247cbd4e424
Víctor Franco <victorfranco5@outlook.com>
2022-09-21 00:07:11 -0300

    challenge completed

--- src/index.js
+++ src/index.js
@@ -10,19 +10,59 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const user = users.find(user => user.username === username)
+
+  if(!user) return response.status(404).json({error: "User not found"})
+
+  request.user = user
+
+  next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if(!user.pro){
+    if(user.todos.length < 10){
+      next()
+    } else{
+      return response.status(403).json({error: "Free plan todo limit reached"})
+    }
+  }
+
+  next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  if(!validate(id)) return response.status(400).json({error: "Todo ID is not valid"})
+
+  const user = users.find(user => user.username === username)
+
+  if(!user) return response.status(404).json({error: "User not found"})
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if(!todo) return response.status(404).json({error: "Todo not found"})
+
+  request.user = user
+  request.todo = todo
+
+  next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+  const user = users.find(user => user.id === id)
+
+  if(!user) return response.status(404).json({error: "User not found"})
+
+  request.user = user
+
+  next()
 }
 
 app.post('/users', (request, response) => {
